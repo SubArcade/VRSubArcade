b1e65c77f9440061453fb546339365d30a3c6160
김홍겸 <gyeom3157@daum.net>
2025-09-01 14:34:07 +0900

    총 , 기본 움직임

---
+++ Assets/_Project/Scripts/Shooting_Khg/Gun.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public Animation anim;
+    //public ParticleSystem muzzle;
+
+    public float rpm; // 분당 발사 횟수
+    private float interval; //발사 간 시간간격;
+
+    private void Awake()
+    {
+        if (false == anim) anim = GetComponent<Animation>();
+        //if (false == muzzle) muzzle = GetComponent<ParticleSystem>();
+
+        float rps = rpm / 60; //초당 발사 횟수
+        interval = 1 / rps;
+    }
+
+
+    float fireTime; // 직전에 발사가 호출된 시간
+    bool isTriggerOn;
+    private void Update()
+    {
+        if (!isTriggerOn) return;
+        if (Time.time > fireTime + interval) return; //쿨 안돌았으면 리턴
+        fireTime = Time.time;
+
+
+        Fire();
+    }
+
+    private void Fire()
+    {
+    }
+
+    public void PullTrigger(bool isOn)
+    {
+        isTriggerOn = isOn;
+
+        if (isOn) { 
+            anim.Play(); 
+            //muzzle.Play();
+        } //총알 발사
+        else { 
+            anim.Stop(); 
+            //muzzle.Stop();
+        }
+    }
+
+}
---
+++ Assets/_Project/Scripts/Shooting_Khg/Hand.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
+using ABC = UnityEngine.XR.Interaction.Toolkit.ActionBasedController;
+
+public class Hand : MonoBehaviour
+{
+    public ABC abc; //햅틱 진종을 주기 위해 컨트롤로 컴포넌트 참조
+    public Gun gun; //총에 트리거 효과를 주기 위해 참조
+
+    public InputActionProperty triggerButton;
+
+
+    private void Awake()
+    {
+        if(!abc) abc = GetComponent<ABC>();
+        if(!gun) gun = GetComponent<Gun>();
+
+        triggerButton.EnableDirectAction(); //확장메서드 
+    }
+
+
+    void OnEnable()
+    {
+        triggerButton.action.performed += OnTriggerInput;
+    }
+
+    void OnDisable()
+    {
+        triggerButton.action.performed -= OnTriggerInput;
+    }
+
+    void OnDestroy()
+    {
+        triggerButton.DisableDirectAction();    
+    }
+    
+    void OnTriggerInput(InputAction.CallbackContext context)
+    {
+        isFire = context.ReadValueAsButton(); //버튼이 눌렸으면 true , 떨어졌으면 false를 반환해줌
+        
+        print($"트리거 눌림 : {isFire}");
+        gun.PullTrigger( isFire );
+    }
+
+    public bool isFire; //트리거가 눌렸을때 true
+    public float hapticInterval; //햅틱 피드백 간격
+    [Range( 0, 1 )] public float hapticIntensity; //진동 강도
+    public float hapticDuration; //진동 지속시간
+
+    private float timeCache;
+    private void Update()
+    {
+        if( !isFire ) return;
+        if (Time.time < timeCache + hapticInterval) return;
+
+        timeCache = Time.time;
+        SendHaptic();
+    }
+
+    public void SendHaptic() //컨트롤러에 진동 보내는 함수
+    {
+        abc.SendHapticImpulse( hapticIntensity , hapticDuration );
+    }
+}

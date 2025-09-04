98fcf0c154859064d44e24f801aa1c40a5787f19
yybbuu1 <ybu1357@gmail.com>
2025-09-04 13:27:52 +0900

    9/4 주크박스
    게임기나 암데나 넣으면됨/ 8bit노래 에셋 다운 ㄱㄱ

---
+++ Assets/_Project/Scripts/JukeBox.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JukeBox : MonoBehaviour
+{
+    public AudioClip[] bgms;
+    public AudioSource Audio;
+
+    private int currentIndex = 0;
+
+
+    void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+        SongStart();
+    }
+
+    public void SongStart() 
+    {
+        if (bgms.Length == 0) return;
+
+        Audio.clip = bgms[currentIndex];
+        Audio.Play();
+    }
+
+    public void SongStop() 
+    {
+        Audio.Stop();
+    }
+
+    public void SongChange() 
+    {
+        if (bgms.Length == 0) return ;
+
+        currentIndex = Random.Range(0, bgms.Length);
+        Audio.clip = bgms[currentIndex];
+        Audio.Play();
+    }
+}
